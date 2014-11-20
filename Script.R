a7546ab51aa382b87d04bcd096d29c24081aef1d
romozzi8 <Peter@MacBook-Pro-di-Pietro-Romozzi.local>
2014-11-20 19:13:51 +0100

    update post-lab
    file excel with state-of-the-art dataset and script

---
+++ Script.R
@@ -0,0 +1,8 @@
+# Packages
+install.packages("devtools")
+require("devtools")
+install_github("rsdmx", "opensdmx")
+library("rsdmx")
+
+# Building dataset
+# ISTAT website, selection of 2009 data for unemployment rate, regional level, exported
